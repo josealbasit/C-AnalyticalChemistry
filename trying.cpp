abf8d1f5a9571cbde149faff5fcedc4651dcbf11
josealbasit <83695122+josealbasit@users.noreply.github.com>
2022-10-20 12:47:37 +0100

    Add files via upload

---
+++ trying.cpp
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include<iostream>
+#include <math.h>
+#include<cmath>
+#include "src/statistics.h"
+#include<Eigen/Dense>
+#include<fstream>
+#include<iostream>
+
+using namespace Eigen;
+using namespace iostream;
+
+int main()
+{
+	MatrixXd a();
+	Matrix 
+
+
+}
